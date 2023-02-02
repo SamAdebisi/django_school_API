bc9fb4fb195794f229879a75451a50bfe0722141
SamAdebisi <samadebisi12@gmail.com>
2023-02-02 13:58:39 +0100

    environs allowed_hosts

--- school_API/settings.py
+++ school_API/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [env.str("ALLOWED_HOSTS")]
 
 
 # Application definition
