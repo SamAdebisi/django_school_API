de7726760ddeed62cdb0ceebb586bf3c14412c7a
SamAdebisi <samadebisi12@gmail.com>
2023-02-02 19:11:53 +0100

    schools app

--- school_API/settings.py
+++ school_API/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # local
+    "schools.apps.SchoolsConfig",
 ]
 
 MIDDLEWARE = [
