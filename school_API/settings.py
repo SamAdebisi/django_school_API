4a71c3697c2e60c7e3c43d616406e560178fda7c
SamAdebisi <samadebisi12@gmail.com>
2023-02-02 14:01:57 +0100

    environs debug

--- school_API/settings.py
+++ school_API/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")  # os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = env.bool("DEBUG", default=False)  # os.getenv("DEBUG")
 
 ALLOWED_HOSTS = [env.str("ALLOWED_HOSTS")]
 
