dc76bcf7b36ae382233677a0d520af1c18291ed3
SamAdebisi <samadebisi12@gmail.com>
2023-02-02 19:24:29 +0100

    students models

--- schools/models.py
+++ schools/models.py
@@ -9,3 +9,13 @@ class Admin(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = models.Manager()
+
+
+class Students(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    password = models.CharField(max_length=255)
+    gender = models.CharField(max_length=255)
+    profile_pic = models.FileField(max_length=255)
+    address = models.TextField()
