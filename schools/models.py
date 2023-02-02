ae8224216b319f937f58d96c4102d90ed9f2efe6
SamAdebisi <samadebisi12@gmail.com>
2023-02-02 19:18:47 +0100

    admin models

--- schools/models.py
+++ schools/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Admin(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = models.Manager()
