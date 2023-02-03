4ef07cb2a3cd423418b576223350b63da5be77fe
SamAdebisi <samadebisi12@gmail.com>
2023-02-03 22:03:26 +0100

    forms updated

--- schools/forms.py
+++ schools/forms.py
@@ -1,2 +1,10 @@
 from django.contrib.auth.forms import UserChangeForm
 
+from .models import CustomUser
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ('email',)
