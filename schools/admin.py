c08c7a1dd7c7a85320fd236aec96c6bd32bdcea7
SamAdebisi <samadebisi12@gmail.com>
2023-02-03 22:06:46 +0100

    admin

--- schools/admin.py
+++ schools/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-
 from .models import CustomUser
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
 
 admin.site.register(CustomUser)
