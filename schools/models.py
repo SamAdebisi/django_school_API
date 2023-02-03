c6a4a65fe06ba1bee4b199826f854a990e30c792
SamAdebisi <samadebisi12@gmail.com>
2023-02-03 21:44:35 +0100

    customuser updated

--- schools/models.py
+++ schools/models.py
@@ -13,6 +13,10 @@ class CustomUser(AbstractUser):
     user_type_data = ((1, "HOD"), (2, "Staff"), (3, "Student"))
     user_type = models.CharField(default=1, choices=user_type_data,
                                  max_length=10)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = MyUserManager()
 
 
 class AdminHOD(models.Model):
