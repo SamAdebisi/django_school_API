e6d61eab9ec6f1c6ee22c82038884bccb7362ae8
SamAdebisi <samadebisi12@gmail.com>
2023-02-03 14:16:23 +0100

    notificationstudent models

--- schools/models.py
+++ schools/models.py
@@ -113,3 +113,12 @@ class FeedBackStaff(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
+
+
+class NotificationStudent(models.Model):
+    id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Students, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
