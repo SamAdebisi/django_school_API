172045d97fcba9a16c636016d60fefac97ea9ef9
SamAdebisi <samadebisi12@gmail.com>
2023-02-03 12:50:22 +0100

    staffs models

--- schools/models.py
+++ schools/models.py
@@ -11,11 +11,27 @@ class Admin(models.Model):
     objects = models.Manager()
 
 
+class Staffs(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    password = models.CharField(max_length=255)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+
 class Courses(models.Model):
     id = models.AutoField(primary_key=True)
     course_name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Subjects(models.Model):
+    id = models.AutoField(primary_key=True)
+    subject_name = models.CharField(max_length=255)
+    course_id = models.ForeignKey(Courses, on_delete=models.CASCADE)
 
 
 class Students(models.Model):
