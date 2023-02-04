008013405939e10ddcb1e187e2fd7829a503e484
SamAdebisi <samadebisi12@gmail.com>
2023-02-04 08:27:37 +0100

    models updated

--- schools/models.py
+++ schools/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class MyUserManager(BaseUserManager):
@@ -212,3 +214,5 @@ class Events(models.Model):
     event_time = models.TimeField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
+
+
